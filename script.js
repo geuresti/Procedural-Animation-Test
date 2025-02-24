64c65fa0de68b8f5311963fde95c57ffa7db0232
geuresti <gioeuresti123@gmail.com>
2025-02-24 12:36:11 -0600

    Segments now follow each other. Added logic to position items on the edge of the segments to allow the positioning of elements such as eyes

--- script.js
+++ script.js
@@ -22,55 +22,55 @@ addEventListener("resize", function() {
     init();
 });
 
-//function Circle(x, y, dx, dy, radius, color) {
 function Circle(x, y, radius, color) {
+
     this.x = x;
     this.y = y;
-    //this.dx = dx;
-    //this.dy = dy;
     this.radius = radius;
     this.color = color;
 
-    this.followMouse = function(borderOn) {
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.draw(borderOn);
-    }
+    this.follow = function(followX, followY, distanceConstraint, borderOn, hasEyes=false) {
+        
+        var dx;
+        var dy;
 
-    this.followMouseBorder = function(borderOn) {
+        // follow an object from a distance
+        if (distanceConstraint > 0) {
 
-        var dx = this.x - mouse.x;
-        var dy = this.y - mouse.y;
-        var distance = Math.sqrt(dy**2 + dx**2);
+            dx = this.x - followX;
+            dy = this.y - followY;
+            var distance = Math.sqrt(dy**2 + dx**2);
 
-       // if (dist + this.radius > distanceConstraint) {
-        if (distance > distanceConstraint) {
+            // the circle has left the constrained area
+            if (distance > distanceConstraint) {
 
-            console.log("circle has left the border");
+                if (difference !== 0) {
+                    var dirX = dx / distance;
+                    var dirY = dy / distance;
 
-            if (difference !== 0) {
-                var dirX = dx / distance;
-                var dirY = dy / distance;
+                    var difference = distanceConstraint - distance;
+                    
+                    // linear interpolation factor, smooth follow
+                    /*var lerpFactor = 0.05;
+                    this.x += (followX + difference * dirX - this.x) * lerpFactor;
+                    this.y += (followY + difference * dirY - this.y) * lerpFactor;*/
 
-                var difference = distanceConstraint - distance;
+                    this.x += difference * dirX;
+                    this.y += difference * dirY;
+                } 
+            }
 
-                // linear interpolation factor
-                var lerpFactor = 0.05;
-
-                this.x += (mouse.x + difference * dirX - this.x) * lerpFactor;
-                this.y += (mouse.y + difference * dirY - this.y) * lerpFactor;
-            } 
-
+        // if no distance constraint was provided, follow directly
+        } else {
+            this.x = followX;
+            this.y = followY;
         }
 
-        this.draw(borderOn);
+        this.draw(borderOn, hasEyes, dx, dy);
+
     }
 
-    this.update = function(borderOn) {
-        this.draw(borderOn);
-    }
-
-    this.draw = function(borderOn) {
+    this.draw = function(borderOn, hasEyes=false, dx, dy) {
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -84,37 +84,76 @@ function Circle(x, y, radius, color) {
         }
         
         ctx.closePath();
+
+        if (hasEyes) {
+
+            // use parametric equation to locate the sides of the segment
+            const thetaLeftEye = Math.atan2(dy, dx) + Math.PI * 0.75;
+            const thetaRightEye = Math.atan2(dy, dx) + Math.PI * 1.25;
+
+            const eyeLeftX = this.x + this.radius * Math.cos(thetaLeftEye);
+            const eyeLeftY = this.y + this.radius * Math.sin(thetaLeftEye);
+
+            const eyeRightX = this.x + this.radius * Math.cos(thetaRightEye);
+            const eyeRightY = this.y + this.radius * Math.sin(thetaRightEye);
+
+            ctx.beginPath();
+
+            ctx.arc(eyeLeftX, eyeLeftY, 8, 0, Math.PI * 2, false);
+            ctx.arc(eyeRightX, eyeRightY, 8, 0, Math.PI * 2, false);
+
+            ctx.fillStyle = "orange";
+            ctx.fill();
+            ctx.closePath();
+        }
     };
 }
 
-var mouseBorder;
-var point;
-const distanceConstraint = 75;
+//var circlesArr = [seg3, seg2, seg1, head];
+var circlesArr;
 
 // initialize canvas object (in center of screen)
 function init() {
-    mouseBorder = new Circle(canvas.width / 2, canvas.height / 2, distanceConstraint, '');
-    point = new Circle(canvas.width / 2, canvas.height / 2, 15, '#e7bf79');
+    var head = new Circle(canvas.width / 2, canvas.height / 2, 65, 'purple');
+    var seg1 = new Circle(canvas.width / 2, canvas.height / 2, 75, '#e7bf79');
+    var seg2 = new Circle(canvas.width / 2, canvas.height / 2, 55, 'red');
+    var seg3 = new Circle(canvas.width / 2, canvas.height / 2, 45, 'yellow');
+    var seg4 = new Circle(canvas.width / 2, canvas.height / 2, 35, 'blue');
+
+    circlesArr = [head, seg1, seg2, seg3, seg4];
 }
 
 var cX = canvas.width / 2;
 var cY = canvas.height / 2;
 
-// handles animation, constantly running
 function animate() {
-    // this function should always be called before updating the canvas because:
-    //  browser optimization
-    //  animations in inactive tabs won't play
-    //  better for saving battery
+    // this function should always be called before updating canvas
     requestAnimationFrame(animate);
 
     // clear canvas before drawing next frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.fillText("hello", mouse.x, mouse.y);
 
-    // update the circle object I created
-    mouseBorder.followMouse(true);
-    point.followMouseBorder(false);
+    // having this order of follow commands results in the head 
+    // being drawn on top of the other elements
+    var size = circlesArr.length;
+
+    if (size > 0) {
+
+        for (var i = 0; i < size; i++) {
+
+            segment = circlesArr[i];
+
+            if (i === 0) {
+
+                segment.follow(mouse.x, mouse.y, 65, true, true); 
+
+            } else {
+                prev = circlesArr[i-1];
+                segment.follow(prev.x, prev.y, prev.radius, true);
+            }
+        }
+    }
+
 }
 
 init();
